1502e34dd7c5eb8a3857da412c3e01ed482835a8
sunliang <ofts7th@163.com>
2019-10-30 17:12:21 +0800

    add files

---
+++ winClient/YoudaProxy/Page_Settings.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using YoudaProxy.lib;
+
+namespace YoudaProxy
+{
+    /// <summary>
+    /// Page_AutoUpdate.xaml 的交互逻辑
+    /// </summary>
+    public partial class Page_Settings : Page
+    {
+
+        public Page_Settings()
+        {
+            InitializeComponent();
+        }
+
+        MyConfig cfgServer = null;
+        MyConfig cfgUserToken = null;
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            cfgServer = MyManager.GetConfig("server");
+            if (cfgServer == null)
+            {
+                cfgServer = new MyConfig();
+                cfgServer.MyKey = "server";
+            }
+            cfgUserToken = MyManager.GetConfig("usertoken");
+            if (cfgUserToken == null)
+            {
+                cfgUserToken = new MyConfig();
+                cfgUserToken.MyKey = "usertoken";
+            }
+            this.txtServer.Text = cfgServer.MyVal;
+            this.txtUserToken.Text = cfgUserToken.MyVal;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            cfgServer.MyVal = this.txtServer.Text;
+            cfgUserToken.MyVal = this.txtUserToken.Text;
+            MyManager.SaveConfig(cfgServer);
+            MyManager.SaveConfig(cfgUserToken);
+            NavigationService.GoBack();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ winClient/YoudaProxy/lib/Util.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoudaProxy.lib
+{
+    public class Util
+    {
+        public static string getWebData(string url)
+        {
+            HttpWebRequest webReq = null;
+            try
+            {
+                webReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                webReq.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                return ret;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
